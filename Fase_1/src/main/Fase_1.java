c77d60e2a78124216afbc57930e3cc96e441e584
Mario Marroquín <mariomarroquin1905@gmail.com>
2024-06-15 02:00:09 -0600

    archivo gramática

--- Fase_1/src/main/Fase_1.java
+++ Fase_1/src/main/Fase_1.java
@@ -24,28 +24,78 @@ public class Fase_1 {
     public static void main(String[] args) {
         
         String entrada = """
-                var j: int = 0;
-                var k: int = 10;
-                while (j <= k) {
-                    if (j == 5) {
-                        println("Continuar cuando j es 5");
-                        j++;
-                        continue;
-                    }
-                    match j {
-                        1 => { println("j es 1"); }
-                        2 => { println("j es 2"); }
-                        3 => { println("j es 3"); }
-                        4 => { println("j es 4"); }
-                        6 => { println("j es 6"); }
-                        7 => { println("j es 7"); }
-                        8 => { println("j es 8"); }
-                        9 => { println("j es 9"); }
-                        10 => { println("j es 10"); }
-                        _ => { println("j es otro valor"); }
-                    }
-                    j++;
-                }
+const nota: int = 100;
+println("Operaciones aritmeticas");
+var cadena: string = "Vamos " + "por " + "ese " + nota;
+println(cadena);
+var operaciones: double = 1 + 9 - 8 * 2 / 4 % 2 + (5 ** 2);
+println("El resultado de las operaciones es " + operaciones);
+
+println("");
+
+println("Operaciones Relacionales");
+var a: bool = 1 < 2;
+println("El valor de a es " + a);
+
+var b: bool = 1 == 2;
+println("El valor de b es " + b);
+
+println("");
+
+println("Operaciones Logicas");
+var c: bool = !!true;
+println("El valor de c es " + c);
+
+var d: bool = (true && false) || (false || false);
+println("El valor de d es " + d);
+
+println("");
+
+println("Operaciones combinadas");
+var e: bool = (1 == 2) || (10 < 5) || (!false);
+println("El valor de e es " + e);
+
+println("");
+println("Ciclos, condicionales y saltos de control");
+var i: int = 0;
+while (i < 11) {
+    if (i == 10) {
+        println("Terminamos con break");
+        break;
+    }
+    println("Iteracion " + i);
+    i = i + 1;
+}
+println("");
+for (i = 0; i <= 5; i++) {
+    println("" + 2 + " * " + i + " = " + 2 * i);
+    if (i >= 2) {
+        continue;
+    }
+    println("Solo se imprime 2 veces");
+}
+
+println("");
+
+var j: int = 0;
+var k: int = 10;
+while (j <= k) {
+    match j {
+        1 => { println("j es 1"); }
+        2 => { println("j es 2"); }
+        3 => { println("j es 3"); }
+        4 => { println("j es 4"); }
+        5 => { println("j es 5"); }
+        6 => { println("j es 6"); }
+        7 => { println("j es 7"); }
+        8 => { println("j es 8"); }
+        9 => { println("j es 9"); }
+        10 => { println("j es 10"); }
+        _ => { println("j es otro valor"); }
+    }
+    j++;
+}
+
                         """;
         
         // Generar Analizadores
---
+++ Readme.md
@@ -0,0 +1,6 @@
+# <p align = "center">  **OLC1_VJ24_202110509**
+
+## <p align = "center"> **JavaCraft**
+
+### 📌 Archivo Gramática:
+[Gramatica.txt](Documentacion/Gramatica.txt)
