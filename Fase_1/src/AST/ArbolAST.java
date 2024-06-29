8e2b399ae82a822be52ce0a90dcea4122972f694
Mario Marroquín <mariomarroquin1905@gmail.com>
2024-06-29 17:10:58 -0600

    FASE 2

--- Fase_1/src/AST/ArbolAST.java
+++ Fase_1/src/AST/ArbolAST.java
@@ -22,7 +22,7 @@ public class ArbolAST {
 
         
         // Rutas de los archivos
-        String dotRuta = "Fase_1/Reports/arbolAST.dot";
+        String dotRuta = "arbolAST.dot";
         String svgRuta = "arbolAST.svg";
 
         try (FileWriter writer = new FileWriter(dotRuta)) {
